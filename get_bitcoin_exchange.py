203a522ca11a95c3f3d91cd6c222176e0c5b38d9
Haitham00n <haitham.ahmed@baaz.com>
2018-04-20 09:32:22 +0300

    trivial push - nothing changed

--- get_bitcoin_exchange.py
+++ get_bitcoin_exchange.py
@@ -1,4 +1,4 @@
 import requests
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 print(response.content)
-print("Program run successfully")
+print("Program run successfully again")
