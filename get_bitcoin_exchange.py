4080b678b27da63ad6f403e3b488132bfe23f6e6
Haitham00n <haitham.ahmed@baaz.com>
2018-04-22 14:16:09 +0300

    adding a dublicate line

--- get_bitcoin_exchange.py
+++ get_bitcoin_exchange.py
@@ -2,3 +2,4 @@ import requests
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 print(response.content)
 print("Program run successfully")
+print("Program run successfully")
